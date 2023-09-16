9cc0c8263cf0ba8229f74c6c0e48e26597e0edb0
manishr <manishranjan.jha1979@gmail.com>
2023-09-16 21:25:31 +0100

    ad

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Demo of ReactJS... manish shukla  
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
